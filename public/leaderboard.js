997e04a99deb550ff7408cc45061422244029644
Ayusht20 <ayushtrilokchandani2005@gmail.com>
2026-05-05 16:38:19 +0530

    Changes made!

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -178,9 +178,9 @@ async function loadLeaderboard() {
         loadingMessage.textContent = "Network error: Could not connect to server.";
     }
 }
-window.loadLeaderboard=loadLeaderboard;
 // Initial event listeners and load call
 document.addEventListener('DOMContentLoaded', () => {
     // Initial load
     loadLeaderboard();
 });
+    window.loadLeaderboard=loadLeaderboard;
